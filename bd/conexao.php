401ecefb008578fa860d9b3b4d0c2a37d4a32b86
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-01-05 22:34:08 -0300

    [feat] Upload de pastas como BD e CSS

---
+++ bd/conexao.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'config.php';
+
+class conexao{
+    private static $pdo;
+
+    private function __construct() {
+
+    }
+    private static function verificaExtensao(){
+        $extensao = 'pdo_mysql';
+    }
+    public static function getInstance(){
+
+        self::verificaExtensao();
+        if(!isset(self::$pdo)){
+            try{              
+                self::$pdo = new PDO('mysql:host=' . HOST . ':' . PORT . ';dbname=' . DBNAME . ';charset=' . CHARSET . '', USER, PASSWORD);
+                // Configuração adicional: lança exceções em caso de erro
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);                
+            } catch (PDOException $e) {
+                print "Erro: " . $e->getMessage();
+            }
+        }
+        return self::$pdo;
+    }
+    public static function isConectado(){
+        
+        if(self::$pdo):
+            return true;
+        else:
+            return false;
+        endif;
+    }
+ 
+ }
---
+++ bd/config.php
@@ -0,0 +1,8 @@
+<?php
+define('LOCAL_PATH', 'http://localhost/blog/');
+define('HOST', 'localhost');
+define('PORT', '3306');
+define('DBNAME', 'blog');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8');
