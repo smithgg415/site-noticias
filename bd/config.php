f52e8a4f055608456ac9594c8eb145ddd044db11
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-01-16 00:37:59 -0300

    [fix] DBNAME noticia

--- bd/config.php
+++ bd/config.php
@@ -2,7 +2,7 @@
 define('LOCAL_PATH', 'http://localhost/blog/');
 define('HOST', 'localhost');
 define('PORT', '3306');
-define('DBNAME', 'blog');
+define('DBNAME', 'noticia');
 define('USER', 'root');
 define('PASSWORD', '');
 define('CHARSET', 'utf8');
